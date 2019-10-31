79463a83990e2c094af945fdd79f938eaa0f8bfa
Pere <pere.solaclaver@gmail.com>
2019-10-31 10:52:52 +0000

    build

---
+++ client/build/precache-manifest.94788b9d596e4a2ece7075eced6f5bb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20ac61164b2128be17390656871c782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1caad9533dfa380c813",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a0c5bbda8ff2c8d56b28",
+    "url": "/static/js/2.15f6bf50.chunk.js"
+  },
+  {
+    "revision": "f1caad9533dfa380c813",
+    "url": "/static/js/main.9a6aa517.chunk.js"
+  },
+  {
+    "revision": "008297665725a21f8a9b",
+    "url": "/static/js/runtime-main.3e103e93.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
